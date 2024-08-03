7ed17fe2acb131af0982f478e4352df07c1026c8
caquli1 <aliwkaplanet66@gmail.com>
2024-08-03 17:07:50 +0400

    just for check modal

--- pages/myworlds/pics/WTC/WTC.js
+++ pages/myworlds/pics/WTC/WTC.js
@@ -6,21 +6,17 @@ var images = document.getElementsByClassName('myImages');
 var modalImg = document.getElementById("img01");
 // and the caption in the modal
 var captionText = document.getElementById("caption");
-
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
   // and attach our click listener for this image.
   img.onclick = function(evt) {
-    console.log(evt);
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
   }
 }
-
 var span = document.getElementsByClassName("close")[0];
-
 span.onclick = function() {
   modal.style.display = "none";
 }
